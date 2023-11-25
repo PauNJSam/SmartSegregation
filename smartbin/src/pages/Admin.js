5814996d97f7ed29fda38b62ca7f3a577965bbf8
PauNJSam <pnj5sambueno@gmail.com>
2023-11-25 17:42:26 +0800

    CheckPoints working no css yet

--- smartbin/src/pages/Admin.js
+++ smartbin/src/pages/Admin.js
@@ -1,5 +1,17 @@
+import { useRef, useState } from 'react';
+import { db } from '../config/firebaseConfig';   
+import { set, ref, onValue, remove } from 'firebase/database';
 
 const Admin = () => {
+
+    const write = () => {
+    /*     const uuid = 'USERS';
+        set(ref(db, `/${uuid}`), {
+            studentId: userUIDRef.current.value,
+            points: 123
+        })*/
+    }; 
+
     return(
         <div className='Admin'>
             <p>This is the Admin Page</p>
--- smartbin/src/pages/CheckPoints.js
+++ smartbin/src/pages/CheckPoints.js
@@ -1,20 +1,68 @@
+import { useRef, useState, useEffect } from 'react';
 import { db } from '../config/firebaseConfig';   
 import { set, ref, onValue, remove } from 'firebase/database';
+import { getDatabase, child, get } from "firebase/database";
 
 const CheckPoints = () => {
+    const [userPoints, setUserPoints] = useState(null);
+    const userUIDRef = useRef();
+    const [warning, setWarning] = useState(false);
+    
 
-    const write = () => {
-        const uuid = 'randomuuid';
-        set(ref(db, `/${uuid}`), {
-            studentId: '18-0374-953',
-            points: 123
-        })
+    const readPoints = async () => {
+        
+        try{
+            const pointsRef = ref(db, `${userUIDRef.current.value}/`, 'studentID');
+                onValue(pointsRef, (snapshot) => {
+                    if(snapshot.val()!==null){
+                        const data = snapshot.val();
+                        setUserPoints(data.points);
+                        setWarning(false);
+                    }else{
+                        setWarning(true);
+                        setUserPoints(null);
+                    }
+                });
+        }catch(err){
+            console.log(err.message);
+        }
+        
+        // const dbRef = ref(getDatabase());
+        /* get(child(db, `${userUIDRef.current.value}/`, 'studentID')).then((snapshot) => {
+        if (snapshot.exists()) {
+            console.log(snapshot.val());
+        } else {
+            console.log("No data available");
+        }
+        }).catch((error) => {
+        console.error(error);
+        }); */
+
     };
+
+    
 
     return(
         <div>
-            <p>The Check Your Points</p>
-            <button type="button" onClick={write}>Write to Database</button>
+            <p>The Check How Much Points You Earned!</p>
+            <input type='text' ref={userUIDRef} />
+            <button type="button" onClick={readPoints}>Check</button>
+            <div>
+                <p>You have a total of</p>
+                {
+                    userPoints == null ? null : <p>{userPoints}</p>
+                }
+                {
+                    warning && <p>warning</p>
+                }
+                <p>points</p>
+            </div>
+            <div>
+                <p>Prizes to collect</p>
+                <div>
+
+                </div>
+            </div>
         </div>
     );
 };
